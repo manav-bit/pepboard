bdc0b9a57a44bfc52282b99531bd71ada23c0f52
MANAV AHUJA <ahujamanav43@gmail.com>
2022-09-14 22:41:51 +0530

    finalll

--- index.js
+++ index.js
@@ -81,115 +81,7 @@ let body= document.querySelector("body");
 
     
 
-// tool.fillStyle="blue";
-// tool.fillRect(0,0,200,300);
-// tool.strokeStyle="green";
-// tool.strokeRect(30,30,200, 400);
-// tool.fillStyle="black";
-// tool.fillRect(40,40,100,100)
 
-    //pencil working
-
-
-// let boardTop=canvasBoard.getBoundingClientRect().top;
-//     let boardLeft=canvasBoard.getBoundingClientRect().left;
-// let iX,iY,fX,fY;
-
-// for pencil tool 
-
-
-// body.addEventListener("mousedown",function(e){
-   
-//     iX=e.clientX; 
-//     iY=e.clientY;
-//     if(ctool="penciltool"){
-//         drawingMode=true;
-//     tool.beginPath();
-//     tool.moveTo(iX,iY);
-//     }
-// })
-// body.addEventListener("mouseup",function(e){
-//     if(ctool="penciltool"){
-
-//         drawingMode=false;
-//     }
-//     else if(ctool=="recttool"|| ctool=="linetool") { 
-
-//         fX=e.clientX+boardLeft;
-// fY=e.clientY-boardTop;
-// let height=fY-iY;
-// let width=fX-iX;
-
-// console.log(fX,fY,width,height);
-// if(ctool=="recttool"){
-    
-// tool.strokeRect(iX,iY,width,height);
-// console.log("rectangle tool is selected");
-// }
-// else {
- 
-//     tool.beginPath();
-//   tool.moveTo(iX,iY);
-//     tool.lineTo(fX,fY);
-//     tool.stroke();
-//   console.log("line is selected");
-// }
-
-//     }
-// })
-// body.addEventListener("mousemove",function(e){
-//     if(drawingMode==false){
-//     return;
-//     }
-//   if(ctool=="penciltool") 
-//   { fX = e.clientX - boardLeft;
-//         fY = e.clientY - boardTop;
-//         tool.lineTo(fX, fY);
-//         tool.stroke();
-//         iX=fX;
-//         iY=fY;
-//    }
-       
-   
-// })
-
- 
-// body.addEventListener("mousedown",function(p){//e is the object
-//     // alert("mouse was pressed down");
-// iX=p.clientX+boardLeft;
-// iY=p.clientY-boardTop;
-// console.log(iY,iX);
-
-// })
-
-
-
-// body.addEventListener("mouseup",function(e){//e is the object
-//     // alert("mouse was pressed down");
-//     if(ctool=="recttool" || ctool=="linetool"){
-
-    
-// fX=e.clientX+boardLeft;
-// fY=e.clientY-boardTop;
-// let height=fY-iY;
-// let width=fX-iX;
-
-// console.log(fX,fY,width,height);
-// if(ctool=="recttool"){
-    
-// tool.strokeRect(iX,iY,width,height);
-// console.log("rectangle tool is selected");
-// }
-// else if(ctool=="linetool") {
- 
-//     tool.beginPath();
-//   tool.moveTo(iX,iY);
-//     tool.lineTo(fX,fY);
-//     tool.stroke();
-//   console.log("line is selected");
-// }
-//     }
-// })
 
 
 //for icon pallets
@@ -205,6 +97,13 @@ greenpower.addEventListener("click",function(){
 })
 bluepower.addEventListener("click",function(){
     tool.strokeStyle="blue";
+})
+// eraser
+let eraserr=document.querySelector(".fa-eraser");
+eraserr.addEventListener("click",function(){
+    console.log("clicked");
+    ctool="penciltool";
+    tool.strokeStyle="white";
 })
 // custom color 
 let customcolor=document.getElementById("color");
@@ -255,9 +154,6 @@ eraser.addEventListener("click",function(e){
 }
 })
 
-//  close.addEventListener("dblclick",function(e){
-//     options[2].style.display="flex";  
-//      console.log("clicked")
-//  })   
+
 
    
